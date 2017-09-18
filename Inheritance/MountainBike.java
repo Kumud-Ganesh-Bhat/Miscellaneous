dad0f0e4de22c455478f328cd3ae1ec58354a0b3
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 12:55:46 -0400

    Update MountainBike.java

--- Inheritance/MountainBike.java
+++ Inheritance/MountainBike.java
@@ -1,1 +1,27 @@
+/**
+* <h1> Child class inheriting features of parent class Bike </h1>
+*
+* @author "GeeksForGeeks"
+* @version 1.0
+*
+*/
 
+public class MountainBike extends Bike{
+      
+       int seatHeight;
+  
+       public MountainBike(int speed, int noGears, int seatHeight){
+             super(speed, noGears);
+             this.seatHeight = seatHeight;
+       }
+  
+      public void setHeight(int newValue){
+             seatHeight = newValue;
+      }
+  
+     public String toString(){
+            return(super.toString() + "Seat Height:" + " " + this.seatHeight);
+     }
+  
+}
+      
