da484af455e17392e4481b2426c597938351fc14
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 16:35:26 -0400

    Update AddConcat.java

--- AddConcat.java
+++ AddConcat.java
@@ -1,1 +1,17 @@
+/**
+* <h1> Java Program to demonstrate addition vs Concatenation </h1>
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+public class AddConcat{
+    public static void main(String[] args){
+       int x = 5;
+       int y = 10;
+       System.out.println("Concatenation"+x+y); //prints 510
+       System.out.println("Addition"+(x+y)); //prints 15
+    }
+}
