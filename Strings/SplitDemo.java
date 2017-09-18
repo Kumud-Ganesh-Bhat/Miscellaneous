98f1559597837012f0c4512904d7413830c32d9d
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 18:16:51 -0400

    Update SplitDemo.java

--- Strings/SplitDemo.java
+++ Strings/SplitDemo.java
@@ -1,1 +1,22 @@
+/**
+* <h1> Java program to demonstrate the use of split() method in Strings </h1>
+* split() method is used to split the string into tokens around matches of regular experession
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+public class SplitDemo{
+     public static void main(String[] args){
+          Stirng str = "Geeks for Geeks";
+          String[] tokens = str.split("s", 2);
+          for(String s: token)
+             System.out.println(s);
+     }
+}
+
+//Output
+//Geek
+//for Geeks
