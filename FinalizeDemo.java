ad2e8e9dfc7336b4f02a44d57814b791e244d27b
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 15:41:48 -0400

    Update FinalizeDemo.java

--- FinalizeDemo.java
+++ FinalizeDemo.java
@@ -1,1 +1,25 @@
+/**
+* <h1> Java Program demonstrating the usage of finalize method </h1>
+* finalize method is called by Garbage Collector on an object meant to be dereferenced
+* Even though object is eligible for Garbage Collection multiple times, finalize method can be called only once on an object
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "Geeks For Geeks"
+*
+*/
 
+public class FinalizeDemo{
+  
+    public static void main(String[] args){
+          FinalizeDemo fd = new FinalizeDemo();
+          System.out.println(fd.toString() + " " + fd.hashCode());
+          fd = null;
+          finalize();
+          System.gc();
+    }
+  
+    protected void finalize(){
+         System.out.println("End");
+    }
+}
