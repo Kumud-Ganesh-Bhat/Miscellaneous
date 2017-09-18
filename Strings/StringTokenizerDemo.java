0aec9cc80d540906f35a9c14ecd0f5284367a1fb
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 17:49:14 -0400

    Update StringTokenizerDemo.java

--- Strings/StringTokenizerDemo.java
+++ Strings/StringTokenizerDemo.java
@@ -1,1 +1,17 @@
+/**
+* <h1> StringTokenizer class is used to split the string into tokens based on the delimiter </h1>
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+public class StringTokenizerDemo{
+     public static void main(String[] args){
+         String str = "Java Programming Language";
+         StringTokenizer st = new StringTokenizer(str);
+         while(st.hasNextToken())
+              System.out.println(st.nextToken());
+     }
+}
