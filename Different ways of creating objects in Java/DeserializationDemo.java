ac5a3cdc1aa046cc6f9ea4e8d008df130e172c4b
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 11:48:35 -0400

    Update DeserializationDemo.java

--- Different ways of creating objects in Java/DeserializationDemo.java
+++ Different ways of creating objects in Java/DeserializationDemo.java
@@ -1,1 +1,25 @@
+/**
+* <h1> Deserialization is a method of reading an object from a saved state in a file </h1>
+* Class should implement Serializable interface
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+import SerializationDemo;
+public class DeserializationDemo implements Serializable{
+       public static void main(String[] args){
+         try{
+              File file = new File("SerializationDemo.java");
+              FileInputStream fis = new FileInputStream(file);
+              ObjectInputStream ois = new ObjectInputStream(fis);
+              System.out.println(ois.readObject().toString());
+         }
+         catch(Exception e){
+              e.printStackTrace();
+         }
+      }
+}
+  
