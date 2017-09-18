b3a6314d4e747dcdb75eebcce4b7db758b457b55
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 19:11:49 -0400

    Update ByteArrayMethod.java

--- Strings/ReverseString/ByteArrayMethod.java
+++ Strings/ReverseString/ByteArrayMethod.java
@@ -1,1 +1,22 @@
+/**
+* <h1> Java program to demonstrate the string reverse </h1>
+* There are 5 different ways to reverse the string in java
+* 2. Converting String to byte array
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+public class ByteArrayMethod{
+       public static void main(String[] args){
+              String str = "Kumud Bhat";
+              System.out.println("Before Reversing:" + " " + str);
+              byte[] strToByte = str.getBytes(); //converting string to byte array
+              byte[] result = new byte[strToByte.length];
+              for(int i=0; i<strToByte.length; i++){
+                    result[i] = strToByte[strToByte.length - i -1];
+              }
+              System.out.println("After Reversing:" + " " + new String(result));
+       }
+}
