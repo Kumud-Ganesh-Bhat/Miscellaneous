d9367f6503957850c962bbdcb63548aa0b5dc132
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 19:18:17 -0400

    Update CharArrayMethod1.java

--- Strings/ReverseString/CharArrayMethod1.java
+++ Strings/ReverseString/CharArrayMethod1.java
@@ -1,1 +1,21 @@
+/**
+* <h1> Reversing a string in Java </h1>
+* 4. Converting String to character array 
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+public class CharArrayMethod1{
+       public static void main(String[] args){
+            String str = "Kumud Bhat";
+            System.out.println("Before Reversing:" + str);
+            char[] strToChar = str.toCharArray(); //converting string to character array
+            System.out.print("After Reversing:" + " ");
+            for(int i=strToChar.length-1; i>=0; i--)
+                System.out.print(strToChar[i]);
+            System.out.println();
+      }
+}
