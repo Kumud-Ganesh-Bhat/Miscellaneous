65091be6dadce43997a7d65b5a53b8b96c9786a4
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 12:04:42 -0400

    Update ReflectiveExample.java

--- Different ways of creating objects in Java/ReflectiveExample.java
+++ Different ways of creating objects in Java/ReflectiveExample.java
@@ -1,1 +1,27 @@
+/**
+* <h1> Creating object using newInstance method of Constructor class. </h1>
+* This is similar to creating object using  newInstance method of Class. 
+* In fact, internally new Instance method of Class uses newInstance method of Constructor class
+* Both are considered as reflective ways of creating objects in Java
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+import java.lang.reflect.Constructor;
+
+public class ReflectiveExample{
+     int x = 10;
+     public static void main(String[] args){
+        try{
+             Constructor<ReflectiveExample> constructor = ReflectiveExample.class.getDeclaredConstructor();
+             ReflectiveExample re = (ReflectiveExample) constructor.newInstance();
+             System.out.println(re.x);
+        }
+        catch(Exception e){
+             e.printStackTrace();
+        }
+     }
+}
