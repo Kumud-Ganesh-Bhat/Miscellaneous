7c0b05e4b118fa0824f332708cce5c76a4b23987
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 13:00:05 -0400

    Update Test.java

--- Inheritance/Test.java
+++ Inheritance/Test.java
@@ -1,1 +1,14 @@
+/**
+* <h1> Driver class for MountainBike and Bike </h1>
+*
+* @author "GeeksForGeeks"
+* @version 1.0
+* 
+*/
 
+public class Test{
+      public static void main(String[] args){
+        Bike b = new MountainBike(5,10,5);
+        System.out.println(b.toString());
+      }
+}
