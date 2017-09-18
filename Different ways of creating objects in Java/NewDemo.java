b42b0860b1b17989ec1ba227e25660618245a4c6
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 11:02:09 -0400

    Update NewDemo.java

--- Different ways of creating objects in Java/NewDemo.java
+++ Different ways of creating objects in Java/NewDemo.java
@@ -1,1 +1,19 @@
+/**
+* <h1> Different ways (5) to create objects in Java </h1>
+* Strictly speaking there is only one way that uses new keyword
+* Rest internally uses new keyword
+* Using new keyword - Most general way of creating objects in Java
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+public class NewDemo{
+    int x =  10;
+  public static void main(String[] args){
+     NewDemo nd = new NewDemo();
+     System.out.println(nd.x);
+  }
+}
