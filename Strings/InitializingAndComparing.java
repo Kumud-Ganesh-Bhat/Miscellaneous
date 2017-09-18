30d795cb4b5028c76fece03ae26868d0956f110b
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 17:26:09 -0400

    Update InitializingAndComparing.java

--- Strings/InitializingAndComparing.java
+++ Strings/InitializingAndComparing.java
@@ -1,1 +1,31 @@
+/**
+* <h1> Java Program to demonstrate String initialization and comparison </h1>
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
 
+public class InitializationAndComparison{
+      public static void main(String[] args){
+            String str1 = "Kumud"; //Direct Initialization
+            String str2 = "Kumud";
+            String str3 = "Kushboo";
+            String str4 = new String("Kumud"); //Dynamic initialization using new operator
+            String str5 = new String("Kumud");
+        
+            //Comparing the equality of contents of a string
+            System.out.println(str1.equals(str2)); //true
+            System.out.println(str2.equals(str4)); //true
+            System.out.println(str1.equals(str3)); //false
+        
+           //Comparing the equality of references 
+           System.out.println(str1 == str2); //true
+           System.out.println(str1 == str4); //false
+           System.out.println(str4 == str5); //false
+      }
+}
+        
+            
+          
