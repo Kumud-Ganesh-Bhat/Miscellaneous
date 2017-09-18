31c73a6b1b3a3c78b927645a834cc1d0269c079a
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 12:50:39 -0400

    Update Bike.java

--- Inheritance/Bike.java
+++ Inheritance/Bike.java
@@ -1,1 +1,27 @@
+/**
+* <h1> Java Program to demonstrate the concept of Inheritance </h1>
+*
+* @author "GeeksForGeeks"
+* @version 1.0
+*
+*/
 
+//Parent or base class
+public class Bike{
+      //instance variables
+      int speed;
+      int noGears;
+      
+      public Bike(int speed, int noGears){
+           this.speed = speed;
+           this.noGears = noGears;
+      }
+  
+     public void increaseSpeed(int value){
+          speed += value;
+     }
+  
+     public void applyBrake(int value){
+          speed-= value;
+     }
+}
