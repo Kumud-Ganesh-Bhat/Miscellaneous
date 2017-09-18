a97030206946f7ae541a98c5aec7b4235a617e23
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 14:07:18 -0400

    Update Sum.java

--- MethodOverLoading/Sum.java
+++ MethodOverLoading/Sum.java
@@ -1,1 +1,31 @@
+/**
+* <h1> Java program to demonstrate the concept of Method Overloading </h1>
+* 
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*/
 
+public class Sum{
+  
+     public static int sum(int x, int y){
+           return (x+y);
+     }
+  
+    public static int sum(int x, int y, int z){
+           return (x+y+z);
+    }
+  
+    public static double sum(double x, double y){
+            return (x+y);
+    }
+  
+    public static void main(String[] args){
+          Sum s = new Sum();
+          System.out.println(s.sum(10,20));
+          System.out.println(s.sum(10,20,30));
+          System.out.println(s.sum(10.0,20.0));
+    }
+ 
+}
+  
