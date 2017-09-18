781f9b184a0a0d26442eb4ca55b57bf280412247
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 16:05:18 -0400

    Update Child.java

--- InstanceInitializerBlock/Child.java
+++ InstanceInitializerBlock/Child.java
@@ -1,1 +1,25 @@
+/**
+* <h1> Instance Initializer Block continued </h1>
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksForGeeks"
+*
+*/
+
+public class Child extends Parent{
+  {
+    System.out.println("Child class Initializer Block 1");
+  }
+  
+  public Child(){
+    super();
+    System.out.println("Child class Constructor");
+  }
+  
+  {
+    System.out.println("Child class Initializer Block 2");
+  }
+}
+
 
