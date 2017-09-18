fe6d96bb3fa1b6ecbc1439feaebb9b81319b5293
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 16:07:30 -0400

    Update Test.java

--- InstanceInitializerBlock/Test.java
+++ InstanceInitializerBlock/Test.java
@@ -1,1 +1,17 @@
+/**
+* <h1> Driver class </h1>
+*
+* @author Kumud Bhat
+* @version 1.0
+* @source/reference "GeeksforGeeks"
+*
+*/
 
+public class Test{
+     public static void main(String[] args){
+        Parent p = new Child();
+     }
+}
+
+/* Output
+   
