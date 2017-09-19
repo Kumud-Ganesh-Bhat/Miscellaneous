5898d07cb657327e946c1b8094999b514739bff4
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-09-18 21:06:47 -0400

    Update RemoveTrailingZeros.java

--- Strings/RemoveTrailingZeros.java
+++ Strings/RemoveTrailingZeros.java
@@ -7,4 +7,15 @@
 *
 */
 
-public class RemovingT
+public class RemoveTrailingZeros{
+       public static void main(String[] args){
+              StringBuilder sbl = new StringBuilder("000012305690"); //use StringBuilder class because String objects are immutable
+              System.out.println("Before Removing zeros" + " " + sbl.toString());
+              int count = 0;
+              while(sbl.charAt(i) == '0'){
+                   count++;
+              }
+             sbl.replace(0,count, " " );
+             System.out.println("After Removing zeros" + " " + sbl);
+       }
+}
